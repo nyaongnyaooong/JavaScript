4cd446f928d9ee724bec15ee7f43de31d6e68910
nyaongnyaooong <luckyyou123@naver.com>
2022-11-15 12:50:30 +0900

    first commit

---
+++ Practice/1.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main() 
+{
+
+    for(int i=0; i<11; i++)
+    {
+        printf("%d\n", 10-i);
+        printf("น฿ป็\n");
+    }
+    
+
+	return 0;
+
+}
+
+
+/*
+    scanf("%d", &n);
+
+    int *a = (int*)malloc(sizeof(int)*n);
+    int *b = (int*)malloc(sizeof(int)*n);
+
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d %d", &a[i], &b[i]);
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d\n", a[i] + b[i]);
+    }
+*/
+
+
---
+++ web1/practice.js
@@ -0,0 +1,5 @@
+//console.log("1"+"1");
+
+
+var str = "abcdefghijk";
+console.log(str.length);
